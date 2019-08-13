cba6d2e383ff55be6154982999c2c31ad006234a
zerockcode <cookieKang@gmail.com>
2019-08-13 17:44:43 +0900

    sync error

--- comm/src/views/Todo/TodoList2.js
+++ comm/src/views/Todo/TodoList2.js
@@ -1,66 +1,82 @@
 import React, {Component} from 'react'
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import axios from 'axios'
+import { async } from 'q';
 
-const User = ({ match }) => <p>{match.params.page}</p>
+class ListComponent extends Component {
+    constructor(){
+        super()
+        console.log("--------")
+    }
+
+    async getServerData(page) {
+        return await axios.get("http://192.168.41.80:8080/todo/pages/"+page).then((data)=>{
+            return data
+        })        
+    }
+
+    render(){
+
+        console.log("list ListComponent render.................")
+        const page = this.props.match.params.page
+        
+
+        const syncFn = () => {
+
+            return this.getServerData(page).then( result => {
+                return result.data.content
+            })
+        }
+        let data =syncFn()
+        console.log(data)
+        console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")  
+        
+        
+        (async () => {
+            let arr= []
+            await data.then( obj => {
+                arr.push(<h1>AAA</h1>)
+            })
+            return arr
+        })()        
+
+        return null
+        
+    }
+}
 
 export default class TodoList2 extends Component {
     
     constructor(props){
         super(props)
         console.log("constructor....todoList2")
-        this.state = {
-            content:[],
-            totalPages:0,
-            loaded:false
-        }    
+ 
     }
 
     componentDidMount() {
         console.log("componentDidMount")
-        this.getData(this.props.match.params.page|| 1)
     }
 
-    getData = (page) => {        
-        
-        axios.get("http://192.168.41.80:8080/todo/pages/"+page)
-        .then(res => {
-            console.log(res.data)
-            this.setState( Object.assign( {},res.data,{loaded:true})  )
-            
-        })        
-    }
 
     render() {
-        console.log("render.................")
+        
 
-        console.log(this.props)
-
-        const {content,totalPages, loaded} = this.state
-
-        const list = content.map( ({tno,title}) => {
-            return (<li key={tno}>{title}</li>)
-        } )
         
         return(
             <div>
                 <ul>
                 <li>
-                    <a href="/todoList2/1">Page 1 </a>
+                    <Link to="/todoList2/1">Page 1 </Link>
                 </li>
                 <li>
-                    <a href="/todoList2/2">Page 2 </a>
+                    <Link to="/todoList2/2">Page 2 </Link>
                 </li>
                 <li>
-                    <a href="/todoList2/3">Page 3 </a>
+                    <Link to="/todoList2/3">Page 3 </Link>
                 </li>
                 </ul>
 
-                <ul>
-                    {list}
-                </ul>
-
-                <Route path="/todoList2/:page" component={User} />
+                <Route path="/todoList2/:page" component={ListComponent} />
         </div>
         )
     }    
